5b42b105798f7e9c303bccea8b9fc3f5a46727d5
YanaKovaleva <139144483+YanaKovaleva@users.noreply.github.com>
2025-09-28 10:28:01 -0400

    Audio and areas

--- audio_stream_player_2d.gd
+++ audio_stream_player_2d.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends AudioStreamPlayer2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -7,9 +7,6 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-func _on_body_entered(body: Node2D) -> void:
-	if body is CharacterBody2D:
-		get_tree().change_scene_to_file("res://end.tscn")
+func _process(delta: float) -> void:
+	if playing == false:
+			playing = true
